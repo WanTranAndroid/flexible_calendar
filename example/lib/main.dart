207678c2a95542db79424cb4eb1b02d0ede670eb
WanTranAndroid <1234>
2021-07-24 21:57:22 +0700

    test

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -60,36 +60,33 @@ class _MyHomePageState extends State<MyHomePage> {
             child: SingleChildScrollView(
               child: Container(
                 height: 500,
-                child:
-                    FlexibleCalendarView(
-                      //limit start date
-                      // minimumDate: isChecking || timeChecking == null ? DateTime.now() : timeChecking?.add(Duration(days: 1)),
-                      //limit end date
-                      // maximumDate: isChecking || timeChecking == null ? DateTime.now() : timeChecking?.add(Duration(days: 1)),
-                      initialStartDate: timeChecking,
-                      initialEndDate: timeCheckout,
-                      startEndDateChange: (DateTime startDateData, DateTime? endDateData) {
-                        setState(() {
-                          timeChecking = startDateData;
-                          timeCheckout = endDateData;
-                        });
-                      },
-                      isConvertDateOfWeekToVietnamese: true,
-                      formatMonthTitle: DateFormat("MM, yyyy"),
-                      textPreMonthTitle: "Tháng",
-                      decorationOfSelectedDay: BoxDecoration(
-                        color: Colors.blue,
-                        borderRadius: BorderRadius.circular(25)
-                      ),
-                      colorDayInAnotherMonth: Colors.grey,
-                      colorInRange: Colors.blue.withOpacity(0.1),
-                      borderBackgroundStartDay: BorderRadius.only(topLeft: Radius.circular(25), bottomLeft: Radius.circular(25)),
-                      borderBackgroundEndDay: BorderRadius.only(topRight: Radius.circular(25), bottomRight: Radius.circular(25)),
-                      formatTitleDateOfWeek: "EEE",
-                      isSelectedOnlyOneDay: false,
-                      colorOfDayInMonth: Colors.black,
-                      colorOfSelectedDay: Colors.white,
-                      styleOfDay: TextStyle(fontSize: 17, fontWeight: FontWeight.w500),
+                child: FlexibleCalendarView(
+                  //limit start date
+                  // minimumDate: isChecking || timeChecking == null ? DateTime.now() : timeChecking?.add(Duration(days: 1)),
+                  //limit end date
+                  // maximumDate: isChecking || timeChecking == null ? DateTime.now() : timeChecking?.add(Duration(days: 1)),
+                  initialStartDate: timeChecking,
+                  initialEndDate: timeCheckout,
+                  startEndDateChange: (DateTime startDateData, DateTime? endDateData) {
+                    setState(() {
+                      timeChecking = startDateData;
+                      timeCheckout = endDateData;
+
+                    });
+                  },
+                  isConvertDateOfWeekToVietnamese: true,
+                  formatMonthTitle: DateFormat("MM, yyyy"),
+                  textPreMonthTitle: "Tháng",
+                  decorationOfSelectedDay: BoxDecoration(color: Colors.blue, borderRadius: BorderRadius.circular(25)),
+                  colorDayInAnotherMonth: Colors.grey,
+                  colorInRange: Colors.blue.withOpacity(0.1),
+                  borderBackgroundStartDay: BorderRadius.only(topLeft: Radius.circular(25), bottomLeft: Radius.circular(25)),
+                  borderBackgroundEndDay: BorderRadius.only(topRight: Radius.circular(25), bottomRight: Radius.circular(25)),
+                  formatTitleDateOfWeek: "EEE",
+                  isSelectedOnlyOneDay: false,
+                  colorOfDayInMonth: Colors.black,
+                  colorOfSelectedDay: Colors.white,
+                  styleOfDay: TextStyle(fontSize: 17, fontWeight: FontWeight.w500),
                 ),
               ),
             ),
